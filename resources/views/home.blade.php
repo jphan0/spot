a3ea4bfc3b91872aa006edce3a3f6cbbb2f641b5
James Phan <jphan@sent.com>
2021-08-22 14:34:19 +1000

    improve seo score

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Search songs available through spotify" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="//unpkg.com/alpinejs" defer></script>
         <title>Spot Search</title>
