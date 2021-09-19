cef35c3217c1fffd996c3b65482d2d44e04900bb
James Phan <jphan@sent.com>
2021-09-19 16:58:31 +1000

    livewire ubuntu fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔥</text></svg>">
         <link rel="alternate icon" href="/favicon.ico">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        @livewireStyles
+        @livewireStyles()
         <script src="//unpkg.com/alpinejs" defer></script>
         <title>Spot Search</title>
     </head>
@@ -17,6 +17,6 @@
             @yield('content')
         </div>
         @include('layouts.footer')
-        @livewireScripts
+        @livewireScripts()
     </body>
 </html>
